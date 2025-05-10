dcc51ebeb41f8e692e7a662f5ac5195e13d687ad
T GOURI SANKAR <107185364+TGouriSankar@users.noreply.github.com>
2025-05-10 08:48:37 +0530

    Create llm_memory_walkthrough_ConversationBufferWindowMemory.py

---
+++ llm_memory_walkthrough_ConversationBufferWindowMemory.py
@@ -0,0 +1,43 @@
+from langchain_core import chat_history
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv 
+load_dotenv()
+import os
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+llm  = ChatGroq(model="qwen-qwq-32b")
+
+from langchain_community.llms import Ollama
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+
+template = """<s><|user|>Current conversation:{chat_history}
+
+{input_prompt}<|end|>
+<|assistant|>"""
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["input_prompt","chat_history"])
+
+
+memory = ConversationBufferWindowMemory(memory_key="chat_history",k=2)
+llm_ollama = Ollama(model=os.getenv("OLLAMA_MODEL","mistral:latest"),base_url=os.getenv("OLLAMA_URL","http://localhost"))
+
+llm_usage = LLMChain(
+    prompt=prompt,
+    llm=llm_ollama,
+    memory=memory
+    )
+
+
+if __name__ == "__main__":
+    while True:
+        user_message = input("Enter Your Query:-  ")
+        if user_message.lower() == "exit":
+            print("Goodbye!")
+            break
+        else:
+            response = llm_usage.invoke({"input_prompt":user_message})
+            print(response['text'])
